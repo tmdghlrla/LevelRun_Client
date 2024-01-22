f85336a649be33137973b76515515910b1fc9b3e
qooqookeke <151480658+qooqookeke@users.noreply.github.com>
2024-01-22 18:07:24 +0900

    add

---
+++ app/src/main/java/com/qooke/levelrunproject/PostingAddActivity.java
@@ -0,0 +1,13 @@
+package com.qooke.levelrunproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PostingAddActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_postingadd);
+    }
+}
