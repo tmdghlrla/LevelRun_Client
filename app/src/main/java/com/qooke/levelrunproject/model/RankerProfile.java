0ca22b94ff9cca4b43c642432a2eb479deebf6d4
qooqookeke <151480658+qooqookeke@users.noreply.github.com>
2024-01-31 15:50:44 +0900

    add

---
+++ app/src/main/java/com/qooke/levelrunproject/model/RankerProfile.java
@@ -0,0 +1,12 @@
+package com.qooke.levelrunproject.model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class RankerProfile {
+
+    public String result;
+    public ArrayList<Ranker> profileItems;
+    public int count;
+
+}
