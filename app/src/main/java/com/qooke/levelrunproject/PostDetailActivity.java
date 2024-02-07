c2d221c99777aec2594945c847c845ad93e07d15
tmdghlrla <151480604+tmdghlrla@users.noreply.github.com>
2024-02-07 22:48:01 +0900

    add

--- app/src/main/java/com/qooke/levelrunproject/PostDetailActivity.java
+++ app/src/main/java/com/qooke/levelrunproject/PostDetailActivity.java
@@ -17,7 +17,6 @@ import com.qooke.levelrunproject.api.NetworkClient;
 import com.qooke.levelrunproject.config.Config;
 import com.qooke.levelrunproject.model.Posting;
 import com.qooke.levelrunproject.model.Ranker;
-import com.qooke.levelrunproject.model.RankerProfile;
 import com.qooke.levelrunproject.model.Res;
 
 import retrofit2.Call;
@@ -63,11 +62,11 @@ public class PostDetailActivity extends AppCompatActivity {
         imgProfile = findViewById(R.id.imgProfile);
         txtRank = findViewById(R.id.txtRank);
         txtLevel = findViewById(R.id.txtLevel);
-        txtNickname = findViewById(R.id.txtLikerNickname);
+        txtNickname = findViewById(R.id.txtNickName);
         imgShare = findViewById(R.id.imgShare);
         imgPhoto = findViewById(R.id.imgPhoto);
         imgLikes = findViewById(R.id.imgLikes);
-        txtLikerNickname = findViewById(R.id.txtLikerNickname);
+        txtLikerNickname = findViewById(R.id.txtNickName);
         txtLikerCnt = findViewById(R.id.txtLikerCnt);
         txtTag = findViewById(R.id.txtTag);
         txtContent = findViewById(R.id.txtContent);
